7d667c82c30aae5b3980103ec7662d77b12bc5c2
Marge <marge.consunji@gmail.com>
2021-10-01 17:41:10 +0800

    Set flex direction to column

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="flex p-8 text-white text-sm tracking-wide justify-center">
+    <footer className="flex flex-col p-8 text-white text-sm tracking-wide justify-center">
       Challenge by&nbsp;<a className="font-bold" href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
     Coded by&nbsp;<a className="font-bold" href="https://github.com/msunji">Marge C.</a>
     </footer>
