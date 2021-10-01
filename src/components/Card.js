448516330b35f9d9f22e07d7860738cbd079dc77
Marge <marge.consunji@gmail.com>
2021-10-01 13:36:35 +0800

    Made card component

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+export const Card = () => {
+  return (
+    <div className="bg-white w-1/4">
+      <div>
+        <div>
+          <p className="font-bold tracking-wide antialiased">Victor Crest</p>
+        </div>
+        <div className="border border-solid border-t-1 border-grey-dark border-opacity-25">
+          <div className="flex flex-row">
+            <div className="stats-col">
+              <p className="font-bold tracking-wider">80k</p>
+              <p>Followers</p>
+            </div>
+            <div className="stats-col">
+              <p className="font-bold tracking-wider">803k</p>
+              <p>Likes</p>
+            </div>
+            <div className="stats-col">
+              <p className="font-bold tracking-wider">1.4k</p>
+              <p>Photos</p>
+            </div>
+          </div>
+        </div>
+      </div>  
+    </div>
+  )
+}
