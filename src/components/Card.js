faff6cb29540b35f3fb71d363c930960d13c003a
Marge <marge.consunji@gmail.com>
2021-10-01 15:13:38 +0800

    Added box shadow

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,26 @@
 export const Card = () => {
   return (
-    <div className="bg-white w-1/4 rounded-2xl h-auto">
+    <div className="bg-white w-1/4 rounded-2xl h-auto shadow">
         <div className="bg-card-bg bg-no-repeat bg-cover h-32 w-full rounded-t-2xl"></div>
         <div>
           <div> 
             <p><span className="font-bold tracking-wide antialiased text-blue-desat">Victor Crest</span><span className="text-blue-dark">26</span></p>
           </div>
-          <div className="text-sm tracking-wider text-blue-dark">London</div>
+          <div className="text-sm text-blue-dark">London</div>
         </div>
         <div className="border border-solid border-t-1 border-grey-dark border-opacity-25 p-5">
           <div className="flex flex-row">
             <div className="stats-col">
               <p className="font-bold tracking-wider">80k</p>
-              <p className="text-grey-dark tracking-widest text-xs">Followers</p>
+              <p className="text-grey-dark text-xs">Followers</p>
             </div>
             <div className="stats-col">
               <p className="font-bold tracking-wider">803k</p>
-              <p className="text-grey-dark tracking-widest text-xs">Likes</p>
+              <p className="text-grey-dark text-xs">Likes</p>
             </div>
             <div className="stats-col">
               <p className="font-bold tracking-wider">1.4k</p>
-              <p className="text-grey-dark tracking-widest text-xs">Photos</p>
+              <p className="text-grey-dark text-xs">Photos</p>
             </div>
           </div>
         </div>
