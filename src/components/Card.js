f60d891397d19505621ae6d38b163f1b26f00557
Marge <marge.consunji@gmail.com>
2021-10-01 14:41:12 +0800

    Styled smaller bits of text (followers, likes, photos)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,30 @@
 export const Card = () => {
   return (
-    <div className="bg-white w-1/4 rounded-2xl">
-      <div>
+    <div className="bg-white w-1/4 rounded-2xl h-auto">
         <div className="bg-card-bg bg-no-repeat bg-cover h-32 w-full rounded-t-2xl"></div>
         <div>
-          <p className="font-bold tracking-wide antialiased">Victor Crest</p>
+          <div> 
+            <p className="font-bold tracking-wide antialiased">Victor Crest</p>
+            <p>26</p>
+          </div>
+          <div>London</div>
         </div>
-        <div className="border border-solid border-t-1 border-grey-dark border-opacity-25">
+        <div className="border border-solid border-t-1 border-grey-dark border-opacity-25 p-5">
           <div className="flex flex-row">
             <div className="stats-col">
               <p className="font-bold tracking-wider">80k</p>
-              <p>Followers</p>
+              <p className="text-grey-dark tracking-widest text-xs">Followers</p>
             </div>
             <div className="stats-col">
               <p className="font-bold tracking-wider">803k</p>
-              <p>Likes</p>
+              <p className="text-grey-dark tracking-widest text-xs">Likes</p>
             </div>
             <div className="stats-col">
               <p className="font-bold tracking-wider">1.4k</p>
-              <p>Photos</p>
+              <p className="text-grey-dark tracking-widest text-xs">Photos</p>
             </div>
           </div>
         </div>
-      </div>  
     </div>
   )
 }
