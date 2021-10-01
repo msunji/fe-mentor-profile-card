732a9a22d77d349c45c86cdec96b9e2dc9668e4f
Marge <marge.consunji@gmail.com>
2021-10-01 16:58:23 +0800

    Removed lg background position class

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Card } from './components/Card';
 
 function App() {
   return (
-    <main className="w-full h-screen flex items-center justify-center bg-no-repeat bg-blobs">
+    <main className="w-full h-screen flex items-center justify-center bg-no-repeat bg-blobs bg-blobPosition lg:bg-blobPosition-lg">
       <Card />
     </main>
   );
