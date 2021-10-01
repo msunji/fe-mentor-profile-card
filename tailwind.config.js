6dc3abc6ed649dbd59bd23a38763968e03acd2d2
Marge <marge.consunji@gmail.com>
2021-10-01 12:49:26 +0800

    Set colour palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,21 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Khumbh Sans", "sans-serif"]
+    colors: {
+      blue: {
+        desat: "hsl(229, 23%, 23%)",
+        dark: "hsl(227, 10%, 46%)"
+      },
+      cyan: {
+        dark: "hsl(185, 75%, 39%)"
+      },
+      grey: {
+        dark: "hsl(0, 0%, 59%)"
       }
     },
+    fontFamily: {
+      sans: ["Khumbh Sans", "sans-serif"]
+    }
   },
   variants: {
     extend: {},
