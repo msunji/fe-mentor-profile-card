135e75b7b5cf7d946156491793b5b0977c4e03ac
Marge <marge.consunji@gmail.com>
2021-10-01 12:38:57 +0800

    Imported fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "@fontsource/kumbh-sans/400.css";
+import "@fontsource/kumbh-sans/700.css";
+import './styles/base.css';
 import App from './App';
 
 ReactDOM.render(
